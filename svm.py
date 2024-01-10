ee8eb0ce76b15ae8e00e601210852bd46161f2e7
Tameeca <71775450+Tameeca@users.noreply.github.com>
2024-01-10 16:50:36 +0000

    Update svm.py

--- svm.py
+++ svm.py
@@ -41,3 +41,49 @@ X = np.asarray(features_dataset.values)
 
 y = np.asarray(dataset['stroke'].values)
 
+print(X)
+print(y)
+
+#Divide dataset into train and test -> Train (X, Y) -- Test (X,Y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+#4088
+print(X_train.shape)
+#1022
+print(X_test.shape)
+#4088
+print(y_train.shape)
+#1022
+print(y_test.shape)
+
+# Replace NaN values with mean of the column
+imputer = SimpleImputer(strategy='mean')
+X_train_imputed = imputer.fit_transform(X_train)
+X_test_imputed = imputer.transform(X_test)
+
+classifier = svm.SVC(kernel='linear', gamma='auto', C=2)
+classifier.fit(X_train, y_train)
+
+y_predict = classifier.predict(X_test)
+
+print(classification_report (y_test, y_predict, zero_division=0))
+
+cm = confusion_matrix(y_test, y_predict)
+print(cm)
+accuracy_score(y_test,y_predict)
+
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = [ListedColormap(('red', 'green'))(i)], label = j)
+plt.title('Stroke')
+plt.xlabel('BMI')
+plt.ylabel('Average Glucose Level')
+plt.legend()
+plt.show()
+plt.show()
